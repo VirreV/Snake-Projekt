fd34f24b98bbeb1dc52c437b4096e62ebc4a3727
VirreV <61963500+VirreV@users.noreply.github.com>
2020-05-11 19:14:10 +0200

    Fixed movement

--- JS/script.js
+++ JS/script.js
@@ -1,14 +1,8 @@
 let gameInterval;
+let interval = 300; //frame ms
 let playArea = document.createElement("div");
 playArea.id = "playArea";
 document.querySelector("body").appendChild(playArea);
-
-for(let i = 0; i < 400; i++){
-    let x = document.createElement("div");
-    x.className = "borderBlock";
-
-    playArea.appendChild(x);
-}
 
 let headPos = [10, 10]; //[column, row]
 let snakeBody = [[10, 10]]; //[column, row]
@@ -23,30 +17,67 @@ function updateSnakeHead(){
     snakeHead.style.gridRow = headPos[1];
 }
 
+function dead(){
+    window.location.href = "gameOver.html";
+}
+
 playArea.appendChild(snakeHead);
 
 function start(){
     if(document.querySelector("#startBtn").innerHTML == "START"){
+        document.addEventListener('keydown', function(event) {
+            if(event.keyCode == 37) {
+                snakeDir = 2;
+            }
+            else if(event.keyCode == 38){
+                snakeDir = 3;
+            }
+            else if(event.keyCode == 39) {
+                snakeDir = 0;
+            }
+            else if(event.keyCode == 40){
+                snakeDir = 1;
+            }
+        });
         document.querySelector("#startBtn").innerHTML = "STOP";
         document.querySelector("#startBtn").classList.toggle("started");
         gameInterval = setInterval(() => {
-            console.log("interval");
-            moveSnakeBody();
             switch(snakeDir){
                 case 0:
-                    headPos[0]++;
+                    if(headPos[0]+1 <= 20){
+                        headPos[0]++;
+                    }
+                    else{
+                        dead();
+                    }
                     break;
                 case 1:
-                    headPos[1]++;
+                    if(headPos[1]+1 <= 20){
+                        headPos[1]++;
+                    }
+                    else{
+                        dead();
+                    }
                     break;
                 case 2:
-                    headPos[0]--;
+                    if(headPos[0]-1 >= 0){
+                        headPos[0]--;
+                    }
+                    else{
+                        dead();
+                    }
                     break;
                 case 3:
-                    headPos[1]--;
+                    if(headPos[1]-1 >= 0){
+                        headPos[1]--;
+                    }
+                    else{
+                        dead();
+                    }
                     break;
             }
-        }, 1000);
+            moveSnakeBody();
+        }, interval);
 
         function moveSnakeBody(){
             for(let i = 0; i < snakeBody.length; i++){
